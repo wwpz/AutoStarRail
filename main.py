45ad27ca26fc0cb3e4b8fc372123c5ebb98a7ef7
wwpz <lingcglib@163.com>
2024-10-15 10:51:22 +0800

    #pycharm .idea

---
+++ main.py
@@ -0,0 +1,62 @@
+import os
+import sys
+import pyuac
+import atexit
+import core.game as game
+
+from core.log import log
+
+# 将当前工作目录设置为程序所在的目录，确保无论从哪里执行，其工作目录都正确设置为程序本身的位置，避免路径错误。
+os.chdir(
+    os.path.dirname(sys.executable) if getattr(sys, 'frozen', False) else os.path.dirname(os.path.abspath(__file__)))
+
+if not pyuac.isUserAdmin():
+    try:
+        pyuac.runAsAdmin(False)
+        sys.exit(0)
+    except Exception:
+        sys.exit(1)
+
+
+def first_run():
+    log.info("启动成功")
+    input("按回车键关闭窗口. . .")
+    sys.exit(0)
+
+
+def run_main_actions():
+    while True:
+        game.start()
+
+
+def main(action=None):
+    first_run()
+
+    # 完整运行
+    if action is None or action == "main":
+        run_main_actions()
+
+    else:
+        log.error(f"未知任务: {action}")
+        input("按回车键关闭窗口. . .")
+        sys.exit(1)
+
+
+# 程序结束时的处理器
+def exit_handler():
+    log.error(f"程序结束...")
+
+
+if __name__ == "__main__":
+    try:
+        atexit.register(exit_handler)
+        main(sys.argv[1]) if len(sys.argv) > 1 else main()
+    except KeyboardInterrupt:
+        log.error("发生错误: 手动强制停止")
+        input("按回车键关闭窗口. . .")
+        sys.exit(1)
+    except Exception as e:
+        log.error("发生错误 {error}".format(error=e))
+        # notif.notify(cfg.notify_template['ErrorOccurred'].format(error=e))
+        input("按回车键关闭窗口. . .")
+        sys.exit(1)
