50c4fc08331c0461b29c6c0aa160f3dbd00e10d6
wwpz <lingcglib@163.com>
2024-12-11 21:09:53 +0800

    加入hzh签到任务

--- tasks/phone/hzh.py
+++ tasks/phone/hzh.py
@@ -1,16 +1,27 @@
-import time
-
 from automation import auto
-from ..template import Template
+from datetime import datetime
+from utils.base_utils import BaseUtils
+from utils.time_utils import TimeUtil
+from .phone_template import PhoneTemplate
 
 
-class Hzh(Template):
+class Hzh(PhoneTemplate):
     def run(self):
-        print("ok")
-        time.sleep(3)
-        print("完成")
-        # if auto.click_element("./res/food_language/reward/email/game_email.png"):
-        #     if auto.click_element("./res/food_language/reward/email/game_email_receive_reward.png"):
-        #         if auto.click_element("./res/food_language/basics/game_blank_close.png"):
-        #             time.sleep(3)
-        #             auto.click_element("./res/food_language/basics/game_close.png")
+        BaseUtils.copy_to_clipboard("华住会")
+        if auto.find_element("./res/phone/basics/home_search.png", is_global=True):
+            auto.press_keys(['ctrl', 'v'])
+        if auto.click_element("./res/phone/hzh/icon.png", is_global=True):
+            TimeUtil.wait_time(10)
+            if auto.click_element("./res/phone/hzh/close.png", is_global=True):
+                self.log.info("关闭了广告")
+                TimeUtil.wait_(1)
+            if auto.click_element("./res/phone/hzh/signin_icon.png", is_global=True):
+                TimeUtil.wait_time(5)
+                if auto.click_element("./res/phone/hzh/signin_skip.png", is_global=True):
+                    self.log.info("关闭了签到首页指导")
+                    TimeUtil.wait_(1)
+                if auto.click_element("./res/phone/hzh/signin.png", is_global=True):
+                    TimeUtil.wait_(1)
+                    if auto.click_element("./res/phone/hzh/signin_true.png", is_global=True, is_save=True):
+                        today_date = datetime.now().strftime('%Y_%m_%d')
+                        self.log.info(f"{today_date}----华住会签到成功")
