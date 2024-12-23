1e5d0693cc53c684d49fc36511057c889b5d5050
wwpz <lingcglib@163.com>
2024-12-23 10:09:21 +0800

    优化流程

--- tasks/phone/mys/mys.py
+++ tasks/phone/mys/mys.py
@@ -29,7 +29,7 @@ class Mys:
         PhoneUtils.open_search()
         BaseUtils.paste("米游社")
         if auto.click_element("./res/phone/mys/icon.png", is_global=True):
-            TimeUtils.wait_(2)
+            TimeUtils.wait_time(2)
             if auto.click_element("./res/phone/basics/select_app.png", is_global=True):
                 if auto.click_element(f"./res/phone/mys/launch_icon{self.count}.png", is_global=True):
                     TimeUtils.wait_time(15)
@@ -41,23 +41,23 @@ class Mys:
             self.count = i
             self.run_once()
             if auto.click_element("./res/phone/mys/mys_home_click.png", is_global=True):
-                TimeUtils.wait_(2)
+                TimeUtils.wait_time(2)
             if auto.click_element("./res/phone/mys/mys_update_no.png", is_global=True):
-                TimeUtils.wait_(2)
+                TimeUtils.wait_time(2)
             if auto.find_element("./res/phone/mys/mys_home.png", is_global=True):
-                TimeUtils.wait_(2)
+                TimeUtils.wait_time(2)
                 for j in range(6):
                     if auto.click_element("./res/phone/mys/star_rail_text1.png",
                                          is_global=True) or auto.click_element(
                         "./res/phone/mys/star_rail_text2.png", is_global=True):
-                        TimeUtils.wait_(5)
-                        if auto.click_element("./res/phone/mys/mys_comment_icon.png", is_global=True):
-                            TimeUtils.wait_(5)
-                            if auto.click_element("./res/phone/mys/mys_like_icon.png", is_global=True):
-                                TimeUtils.wait_(3)
+                        TimeUtils.wait_time(5)
+                        if auto.click_element("./res/phone/mys/mys_like_icon.png", is_global=True):
+                            TimeUtils.wait_time(5)
+                            if auto.click_element("./res/phone/mys/mys_comment_icon.png", is_global=True):
+                                TimeUtils.wait_time(5)
                                 auto.mouse_right_click(self.center_x, self.center_y)
-                                TimeUtils.wait_(3)
+                                TimeUtils.wait_time(5)
                 if auto.click_element("./res/phone/mys/star_rail_signin_icon.png", is_global=True):
-                    TimeUtils.wait_(3)
+                    TimeUtils.wait_time(5)
                     if auto.click_element("./res/phone/mys/star_rail_signin.png", is_global=True):
-                        TimeUtils.wait_(3)
+                        TimeUtils.wait_time(5)
--- tasks/phone/xmsc/xmsc.py
+++ tasks/phone/xmsc/xmsc.py
@@ -9,50 +9,33 @@ from tasks.phone.phone_template import PhoneTemplate
 
 class Xmsc(PhoneTemplate):
     def run(self):
-        self.log.info("开始小米商城签到")
+        i = 0
         BaseUtils.paste("小米商城")
-
-        if not auto.click_element("./res/phone/xmsc/icon.png", is_global=True):
-            self.log.error("未能找到小米商城图标")
-            return
-
-        TimeUtils.wait_time(15)
-
-        if not auto.click_element("./res/phone/xmsc/home_me.png", is_global=True):
-            self.log.error("未能进入小米商城个人主页")
-            return
-
-        TimeUtils.wait_time(5)
-
-        if not auto.click_element("./res/phone/xmsc/gold.png", is_global=True):
-            self.log.error("未能找到金币图标")
-            return
-
-        for attempt in range(3):
-            if self.perform_signin():
-                break
-
-        self.log.info("小米商城签到完成")
+        if auto.click_element("./res/phone/xmsc/icon.png", is_global=True):
+            TimeUtils.wait_time(15)
+            if auto.click_element("./res/phone/xmsc/close.png", is_global=True):
+                TimeUtils.wait_time(5)
+            if auto.click_element("./res/phone/xmsc/home_me.png", is_global=True):
+                TimeUtils.wait_time(5)
+                if auto.click_element("./res/phone/xmsc/gold.png", is_global=True):
+                    TimeUtils.wait_time(5)
+                while True:
+                    i += 1
+                    if auto.click_element("./res/phone/xmsc/go_signin.png", is_global=True):
+                        TimeUtils.wait_time(5)
+                        if auto.click_element("./res/phone/xmsc/signin.png", is_global=True):
+                            today_date = datetime.now().strftime('%Y_%m_%d')
+                            self.log.info(f"{today_date}----小米商城签到成功")
+                            TimeUtils.wait_time(5)
+                        if auto.click_element("./res/phone/xmsc/go_browse.png", is_global=True):
+                            for i in range(3):
+                                auto.mouse_move(self.center_x, self.bottom - 100)
+                                random_float = random.uniform(250, 450)
+                                auto.mouse_dragRel(0, -random_float, 0.8)
+                                TimeUtils.wait_time(5)
+                        if auto.click_element("./res/phone/xmsc/receive_award.png", is_global=True):
+                            TimeUtils.wait_time(5)
+                        if i == 3:
+                            if auto.find_element("./res/phone/xmsc/done.png", is_global=True):
+                                break
         PhoneUtils.update_or_del_node("小米商城", delete_key="小米商城签到")
-
-    def perform_signin(self):
-        if auto.click_element("./res/phone/xmsc/go_signin.png", is_global=True):
-            TimeUtils.wait_time(5)
-            if auto.click_element("./res/phone/xmsc/signin.png", is_global=True):
-                today_date = datetime.now().strftime('%Y_%m_%d')
-                self.log.info(f"{today_date}----小米商城签到成功")
-                TimeUtils.wait_time(3)
-                return True
-        return False
-
-    def browse_items(self):
-        if auto.click_element("./res/phone/xmsc/go_browse.png", is_global=True):
-            for _ in range(3):
-                auto.mouse_move(self.center_x, self.bottom - 50)
-                random_float = random.uniform(250, 450)
-                auto.mouse_dragRel(0, -random_float, 0.8)
-                TimeUtils.wait_(2)
-
-    def receive_award(self):
-        if auto.click_element("./res/phone/xmsc/receive_award.png", is_global=True):
-            TimeUtils.wait_(3)
